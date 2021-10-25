18e181f1aeccda716598a558b527b0ca8303b1a4
cyberwjulian <cyberwjulian@gmail.com>
2021-10-25 12:48:54 -0400

    Add atm var and if, else statements
    i also changed the w's to vv

--- Control Flow.py
+++ Control Flow.py
@@ -10,15 +10,24 @@ Nesting If statements & refresh our Comparison & Logical Operators
 print("VVelcome to Cash-R-Us Bank\n\nLet's take a moment to set up your account.\n")
 
 # Set up account bu asking users for their first & last names using Variables
-firstName = input("What is your first name: ")
-lastName = input("What is your last name: ")
+firstName = input("VVhat is your first name: ")
+lastName = input("VVhat is your last name: ")
 
-print("\nVVelcome to Cash-R-Us",firstName,lastName+", we will now set up a security PIN on your account.\n")
+print("\nVVelcome to Cash-R-Us",firstName,lastName+", vve vvill now set up a security PIN on your account.\n")
 
 # Set up a PIN - Personal Identification Number
 pin = input("Please choose a 4-digit Personal Identification Number: ")
 
-print("\nThank you", firstName, lastName + ", we see that set you PIN to", pin+".")
+print("\nThank you", firstName, lastName + ", vve see that set you PIN to", pin+".")
+
+print("\nVVould you like to make a transaction through out Automated Teller Machine")
+yon = input("Yes or No: ").upper()
+
+if yon == "YES":
+    print("\n******************************************************************************\n")
 
 
-print("\nWould you like to make a transaction through out Automated Teller Machine")
+
+
+else:
+    print("Have a vvonderful day", firstName, lastName, ". Please come back and visit soon.")
